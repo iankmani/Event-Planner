0587f933dd54b6fd77f807b34422ceaaa82050e1
Ian Kimani <ian.kiarie2002@gmail.com>
2024-07-18 06:12:00 +0300

    add get started page

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import GetStarted from "./Pages/GetStarted/GetStarted";
+function App() {
+  return (
+    <>
+      <GetStarted />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/GetStarted/GetStarted.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./GetStarted.css";
+
+const GetStarted = () => {
+  return (
+    <div className="GetStarted">
+      <div className="GetStarted_container">
+        <div className="GetStarted_title">
+          <h1>Event Planner</h1>
+        </div>
+        <div className="GetStarted_Description">
+          <h4>Where Every Moment Becomes a Memory</h4>
+        </div>
+        <button className="GetStarted_button">Get Started</button>
+      </div>
+    </div>
+  );
+};
+
+export default GetStarted;
